02fc8b9406739f27235dcb8da6f05915e9566fbd
@pongsakorn.sa <pongsakorn.sa@tcc-technology.com>
2025-08-06 15:47:45 +0700

    room_page add more content

--- lib/modules/room_page/pages/room_page.dart
+++ lib/modules/room_page/pages/room_page.dart
@@ -34,7 +34,7 @@ class _RoomPageState extends State<RoomPage> {
             child: Image.asset(
               'assets/images/homepage1.png',
               fit: BoxFit.cover,
-              opacity: const AlwaysStoppedAnimation(0.65), // ปรับโปร่งใส
+              opacity: const AlwaysStoppedAnimation(0.3),
             ),
           ),
           SingleChildScrollView(
@@ -303,7 +303,250 @@ Widget _buildHeader(BuildContext context) {
           ),
         ),
 
-        const SizedBox(height: 16),
+        const SizedBox(height: 7),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.star, size: 20, color: Colors.black),
+                SizedBox(width: 8),
+                Text(
+                  "ชามอาหาร ชามน้ำ",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontFamily: 'Regular',
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.star, size: 20, color: Colors.black),
+                SizedBox(width: 8),
+                Text(
+                  "กระบะทราย พร้อมที่ตักส่วนตัว",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontFamily: 'Regular',
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.star, size: 20, color: Colors.black),
+                SizedBox(width: 8),
+                Text(
+                  "กล่องนอน หลุมใส แผ่นลับเล็บ",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontFamily: 'Regular',
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+
+        const SizedBox(height: 20),
+        const ImageSliderBox(),
+
+        const SizedBox(height: 14),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomElevatedButton(
+              text: 'จองห้องพัก',
+              fontWeight: FontWeight.bold,
+              backgroundColor: const Color(0xFFAB80E3),
+              onPressed: () {},
+              width: 204,
+              height: 54,
+            ),
+          ],
+        ),
+        const SizedBox(height: 80),
+        SizedBox(
+          height: screenHeight * 0.5,
+          child: Stack(
+            children: [
+              Positioned(
+                top: 34,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: Text(
+                    'OPEN ROOM',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontFamily: 'bold',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 80,
+                left: MediaQuery.of(context).size.width / 2 - 273 / 2,
+                child: Container(
+                  width: 273,
+                  height: 353,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white, width: 3),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.asset(
+                      'assets/images/catroom2.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 40,
+                left: 30,
+                child: Container(
+                  width: 84,
+                  height: 84,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(90),
+                    color: Colors.white,
+                  ),
+                  child: Center(
+                    child: Text(
+                      '6 ตัว',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontFamily: 'bold',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 7),
+
+        Container(
+          width: 280,
+          height: 56,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(90),
+            color: Color(0xFFD5BFF1), // แบบ hex
+          ),
+          child: Center(
+            child: Text(
+              'ราคา 300 บาท/คืน',
+              style: TextStyle(
+                fontFamily: 'bold',
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 7),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.star, size: 20, color: Colors.black),
+                SizedBox(width: 8),
+                Text(
+                  "ชามอาหาร ชามน้ำ",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontFamily: 'Regular',
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.star, size: 20, color: Colors.black),
+                SizedBox(width: 8),
+                Text(
+                  "กระบะทราย พร้อมที่ตักส่วนตัว",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontFamily: 'Regular',
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.star, size: 20, color: Colors.black),
+                SizedBox(width: 8),
+                Text(
+                  "กล่องนอน หลุมใส แผ่นลับเล็บ",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontFamily: 'Regular',
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+
+        const SizedBox(height: 20),
+        const ImageSliderBox(),
+
+        const SizedBox(height: 14),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomElevatedButton(
+              text: 'จองห้องพัก',
+              fontWeight: FontWeight.bold,
+              backgroundColor: const Color(0xFFAB80E3),
+              onPressed: () {},
+              width: 204,
+              height: 54,
+            ),
+          ],
+        ),
+        const SizedBox(height: 50),
+          Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomElevatedButton(
+              text: 'ติดต่อสอบถาม',
+              fontWeight: FontWeight.bold,
+              backgroundColor: const Color(0xFFAB80E3),
+              onPressed: () {},
+              borderRadius: 18,
+              width: 260,
+              height: 70,
+            ),
+          ],
+        ),
+        const SizedBox(height: 26),
       ],
     ),
   );
